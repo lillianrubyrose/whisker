787f37e9e6e5e761f136961147d13d89bc3576c8
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2025-02-13 20:21:29 -0500

    clean up instruction parcel extraction and add opcode consts

--- src/insn.rs
+++ src/insn.rs
@@ -64,7 +64,24 @@ struct JType {
 }
 
 /// extracts bits start..=end from val
+fn extract_bits_16(val: u16, start: u8, end: u8) -> u16 {
+	assert!(start <= end);
+	assert!(start < u16::BITS as u8);
+	assert!(end < u16::BITS as u8);
+
+	// masks off the low bits
+	let low_mask = (u16::MAX >> start) << start;
+	// shift off the high bits
+	let high_mask = (u16::MAX << (u16::BITS - u32::from(end) - 1)) >> (u16::BITS - u32::from(end) - 1);
+	(val & low_mask & high_mask) >> start
+}
+
+/// extracts bits start..=end from val
 fn extract_bits_32(val: u32, start: u8, end: u8) -> u32 {
+	assert!(start <= end);
+	assert!(start < u32::BITS as u8);
+	assert!(end < u32::BITS as u8);
+
 	// masks off the low bits
 	let low_mask = (u32::MAX >> start) << start;
 	// shift off the high bits
@@ -128,10 +145,29 @@ impl Instruction {
 	}
 
 	fn parse_32bit_instruction(parcel: u32) -> Instruction {
-		let opcode = (parcel & 0b1111100) >> 2;
+		let opcode = extract_bits_32(parcel, 2, 6);
+		use consts::opcode::*;
 		match opcode {
-			// store
-			0b01000 => {
+			LOAD => unimplemented!("LOAD"),
+			LOAD_FP => unimplemented!("LOAD-FP"),
+			CUSTOM_0 => unimplemented!("CUSTOM-0"),
+			MISC_MEM => unimplemented!("MISC-MEM"),
+			OP_IMM => {
+				let itype = Self::parse_itype(parcel);
+				match itype.func {
+					0b000 => IntInstruction::AddImmediate {
+						dst: itype.dst,
+						src: itype.src,
+						val: itype.imm,
+					}
+					.into(),
+					_ => unreachable!("should've matched op-imm func"),
+				}
+			}
+			AUIPC => unimplemented!("AUIPC"),
+			OP_IMM_32 => unimplemented!("OP-IMM-32"),
+			UNK_48B => unimplemented!("48b"),
+			STORE => {
 				let stype = Self::parse_stype(parcel);
 				match stype.func {
 					// sb
@@ -144,9 +180,11 @@ impl Instruction {
 					_ => unreachable!("couldnt match store stype func"),
 				}
 			}
-
-			// lui
-			0b01101 => {
+			STORE_FP => unimplemented!("STORE-FP"),
+			CUSTOM_1 => unimplemented!("CUSTOM-1"),
+			AMO => unimplemented!("AMO"),
+			OP => unimplemented!("OP"),
+			LUI => {
 				let utype = Self::parse_utype(parcel);
 				IntInstruction::LoadUpperImmediate {
 					dst: utype.dst,
@@ -154,23 +192,20 @@ impl Instruction {
 				}
 				.into()
 			}
-
-			// op-imm
-			0b00100 => {
-				let itype = Self::parse_itype(parcel);
-				match itype.func {
-					0b000 => IntInstruction::AddImmediate {
-						dst: itype.dst,
-						src: itype.src,
-						val: itype.imm,
-					}
-					.into(),
-					_ => unreachable!("should've matched op-imm func"),
-				}
-			}
-
-			// jal
-			0b11011 => {
+			OP_32 => unimplemented!("OP_32"),
+			UNK_64B => unimplemented!("64b"),
+			MADD => unimplemented!("MADD"),
+			MSUB => unimplemented!("MSUB"),
+			NMSUB => unimplemented!("NMSUB"),
+			NMADD => unimplemented!("NMADD"),
+			OP_FP => unimplemented!("OP-FP"),
+			OP_V => unimplemented!("OP-V"),
+			CUSTOM_2 => unimplemented!("CUSTOM-2"),
+			UNK_48B2 => unimplemented!("48b (2)"),
+			BRANCH => unimplemented!("BRANCH"),
+			JALR => unimplemented!("JALR"),
+			RESERVED => unimplemented!("RESERVED"),
+			JAL => {
 				let jtype = Self::parse_jtype(parcel);
 				IntInstruction::JumpAndLink {
 					link_reg: jtype.dst,
@@ -178,8 +213,12 @@ impl Instruction {
 				}
 				.into()
 			}
+			SYSTEM => unimplemented!("SYSTEM"),
+			OP_VE => unimplemented!("OP-VE"),
+			CUSTOM_3 => unimplemented!("CUSTOM-3"),
+			UNK_80B => unimplemented!("80b"),
 
-			_ => unreachable!("should've matched 32bit instruction opcode {parcel:#018b}"),
+			_ => unreachable!("??? opcode {opcode:#07b}"),
 		}
 	}
 
@@ -187,18 +226,62 @@ impl Instruction {
 		assert!(cpu.registers.pc % 2 == 0);
 
 		let parcel1 = cpu.physmem.read_u16(cpu.registers.pc);
-		if parcel1 & 0b11 != 0b11 {
+		if extract_bits_16(parcel1, 0, 1) != 0b11 {
 			todo!("16bit instruction");
-		} else if (parcel1 & 0b11100) >> 2 != 0b111 {
+		} else if extract_bits_16(parcel1, 2, 4) != 0b111 {
 			let full_parcel = cpu.physmem.read_u32(cpu.registers.pc);
 			cpu.registers.pc += 4;
 			Self::parse_32bit_instruction(full_parcel)
-		} else if (parcel1 & 0b111111) == 0b011111 {
+		} else if extract_bits_16(parcel1, 0, 5) == 0b011111 {
 			todo!("48bit instruction");
-		} else if (parcel1 & 0b1111111) == 0b0111111 {
+		} else if extract_bits_16(parcel1, 0, 6) == 0b0111111 {
 			todo!("64bit instruction");
 		} else {
 			unimplemented!("{parcel1:#018b}")
 		}
+	}
+}
+
+mod consts {
+	// ==========================================
+	// opcode types, from ISA volume 1 chapter 34
+	// ==========================================
+	pub(super) mod opcode {
+		pub const LOAD: u32 = 0b00000;
+		pub const LOAD_FP: u32 = 0b000001;
+		pub const CUSTOM_0: u32 = 0b00010;
+		pub const MISC_MEM: u32 = 0b00011;
+		pub const OP_IMM: u32 = 0b00100;
+		pub const AUIPC: u32 = 0b00101;
+		pub const OP_IMM_32: u32 = 0b00110;
+		// FIXME ??? what is this
+		pub const UNK_48B: u32 = 0b00111;
+		pub const STORE: u32 = 0b01000;
+		pub const STORE_FP: u32 = 0b01001;
+		pub const CUSTOM_1: u32 = 0b01010;
+		pub const AMO: u32 = 0b01011;
+		pub const OP: u32 = 0b01100;
+		pub const LUI: u32 = 0b01101;
+		pub const OP_32: u32 = 0b01110;
+		// FIXME ??? what is this
+		pub const UNK_64B: u32 = 0b01111;
+		pub const MADD: u32 = 0b10000;
+		pub const MSUB: u32 = 0b10001;
+		pub const NMSUB: u32 = 0b10010;
+		pub const NMADD: u32 = 0b10011;
+		pub const OP_FP: u32 = 0b10100;
+		pub const OP_V: u32 = 0b10101;
+		pub const CUSTOM_2: u32 = 0b10110;
+		// FIXME ??? what is this
+		pub const UNK_48B2: u32 = 0b10111;
+		pub const BRANCH: u32 = 0b11000;
+		pub const JALR: u32 = 0b11001;
+		pub const RESERVED: u32 = 0b11010;
+		pub const JAL: u32 = 0b11011;
+		pub const SYSTEM: u32 = 0b11100;
+		pub const OP_VE: u32 = 0b11101;
+		pub const CUSTOM_3: u32 = 0b11110;
+		// FIXME ??? what is this
+		pub const UNK_80B: u32 = 0b11111;
 	}
 }

148b67179d01eb4193ff5564c3dcb11de9b06944
Lillian Rose <me@lillianro.se>
2025-02-17 20:30:19 -0500

    Nix: pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:ipetkov/crane";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
@@ -24,14 +28,20 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
 
+      imports = [
+        inputs.git-hooks.flakeModule
+      ];
+
       perSystem = {
         pkgs,
         system,
+        config,
         ...
       }: let
         overlays = [fenix.overlays.default];
         pkgs = import nixpkgs {inherit system overlays;};
         pkgsRiscv = (import nixpkgs {inherit system;}).pkgsCross.riscv64;
+        pre-commit-hooks = inputs.pre-commit-hooks.lib.${system};
         craneLib = crane.mkLib pkgs;
         commonArgs = {
           src = craneLib.cleanCargoSource ./.;
@@ -45,6 +55,23 @@
             cargoArtifacts = craneLib.buildDepsOnly commonArgs;
           });
 
+        pre-commit.settings.hooks = {
+          clippy = {
+            enable = true;
+            packageOverrides = {
+              cargo = pkgs.fenix.stable.cargo;
+              clippy = pkgs.fenix.stable.clippy;
+            };
+          };
+          rustfmt = {
+            enable = true;
+            packageOverrides = {
+              cargo = pkgs.fenix.stable.cargo;
+              rustfmt = pkgs.fenix.stable.rustfmt;
+            };
+          };
+        };
+
         devShells = {
           default = pkgs.mkShell {
             nativeBuildInputs = [
@@ -55,6 +82,10 @@
               pkgsRiscv.buildPackages.gdb
               pkgsRiscv.buildPackages.binutils
             ];
+
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
           };
         };
       };

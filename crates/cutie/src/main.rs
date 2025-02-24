85abb26fd06c77b766a1e09204545e6caa1e3886
Lillian Rose <me@lillianro.se>
2025-02-24 07:15:31 -0500

    Remove old compile command
    New one is much better! <3 @asquared31415

--- crates/cutie/src/main.rs
+++ crates/cutie/src/main.rs
@@ -2,7 +2,6 @@ use std::process::exit;
 use std::{path::PathBuf, process::Command};
 
 use clap::{Parser, Subcommand};
-use cmd_lib::run_cmd;
 use tracing::*;
 use tracing_subscriber::layer::SubscriberExt as _;
 use tracing_subscriber::util::SubscriberInitExt as _;
@@ -17,10 +16,6 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
 	Compile {
-		/// Input file name
-		input: String,
-	},
-	Compile2 {
 		#[arg(short, default_value_t = String::from("boot.bin"))]
 		out: String,
 		files: Vec<PathBuf>,
@@ -34,8 +29,7 @@ fn main() {
 		.init();
 	let args = Args::parse();
 	match args.command {
-		Commands::Compile { input } => compile(&input),
-		Commands::Compile2 { out, files } => compile2(out, files),
+		Commands::Compile { out, files } => compile(out, files),
 	}
 }
 
@@ -50,7 +44,7 @@ fn find_command(options: &[&'static str]) -> Option<&'static str> {
 	None
 }
 
-fn compile2(out_name: String, files: Vec<PathBuf>) {
+fn compile(out_name: String, files: Vec<PathBuf>) {
 	if files.is_empty() {
 		error!("no input files given");
 		exit(1)
@@ -156,55 +150,5 @@ fn compile2(out_name: String, files: Vec<PathBuf>) {
 	info!(
 		"DONE! output binary at `{}`",
 		out_path.strip_prefix(target_dir).unwrap().display()
-	);
-}
-
-fn compile(input: &str) {
-	let base_dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"));
-	let progs_dir = base_dir.join("progs");
-	let target_dir = base_dir.join("target");
-
-	let program_path = progs_dir.join(input);
-	if !PathBuf::from(&program_path).exists() {
-		eprintln!("Error: File '{}' does not exist", program_path.to_string_lossy());
-		std::process::exit(1);
-	}
-
-	let Some(cc) = find_command(&[
-		"riscv64-elf-gcc",
-		"riscv64-unknown-linux-gnu-gcc",
-		"riscv64-unknown-elf-gcc",
-	]) else {
-		eprintln!("Error: No suitable RISC-V toolchain found (Missing GCC).");
-		std::process::exit(1);
-	};
-	let Some(objcopy) = find_command(&[
-		"riscv64-elf-objcopy",
-		"riscv64-unknown-linux-gnu-objcopy",
-		"riscv64-unknown-elf-objcopy",
-	]) else {
-		eprintln!("Error: No suitable RISC-V toolchain found (Missing objcopy).");
-		std::process::exit(1);
-	};
-
-	let base_name = PathBuf::from(input)
-		.file_stem()
-		.expect("Invalid file name")
-		.to_string_lossy()
-		.to_string();
-
-	if let Err(e) = run_cmd!(
-		"$cc" -O0 -march=rv64idc -mcmodel=medany -std=c23 -c "$program_path" -o "$target_dir/$base_name.o" -nostdlib -nodefaultlibs -fno-stack-protector;
-		"$cc" -O0 -march=rv64idc -mcmodel=medany -std=c23 -c "$progs_dir/whisker.c" -o "$target_dir/whisker.o" -nostdlib -nodefaultlibs -fno-stack-protector;
-		"$cc" -O0 -T "linker.ld" -o "$target_dir/$base_name.elf" "$target_dir/$base_name.o" "$target_dir/whisker.o" -nostdlib -nodefaultlibs;
-		"$objcopy" -O binary "$target_dir/$base_name.elf" "$target_dir/$base_name.bin";
-	) {
-		eprintln!("Command execution failed: {}", e);
-		std::process::exit(1);
-	}
-
-	println!(
-		"Compiled '{input}' as bootrom to '{}/{base_name}.bin'",
-		target_dir.to_string_lossy()
 	);
 }

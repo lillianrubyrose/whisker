540f885d5b15759c751ce4b035fd0bbe9642d28e
Lillian Rose <me@lillianro.se>
2025-07-06 19:26:28 -0400

    Fix nix devshell and complete basic ELF parser

--- src/elfie/src/lib.rs
+++ src/elfie/src/lib.rs
@@ -1,4 +1,7 @@
-use std::io::{Cursor, Read, Seek};
+use std::{
+	io::{Cursor, Read, Seek},
+	string,
+};
 
 use crate::ext::ReadExt;
 
@@ -16,14 +19,14 @@ pub enum Class {
 	X64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum ISA {
 	RiscV,
 }
 
 impl ISA {
-	pub fn parse(cursor: &mut Cursor<&[u8]>) -> Result<Self, std::io::Error> {
-		let value = cursor.read_u8()?;
+	pub fn parse(cursor: &mut Cursor<&[u8]>, endianness: Endianness) -> Result<Self, std::io::Error> {
+		let value = cursor.read_16(endianness)?;
 		match value {
 			0xF3 => Ok(ISA::RiscV),
 			_ => Err(std::io::Error::new(
@@ -34,7 +37,8 @@ impl ISA {
 	}
 }
 
-pub struct ProgramHeaderType(i32);
+#[derive(Debug)]
+pub struct ProgramHeaderType(u32);
 
 impl ProgramHeaderType {
 	pub const PT_NULL: Self = Self(0x00000000);
@@ -47,9 +51,10 @@ impl ProgramHeaderType {
 	pub const PT_TLS: Self = Self(0x00000007);
 }
 
+#[derive(Debug)]
 pub struct ProgramHeader {
 	pub ty: ProgramHeaderType,
-	pub flags: i32,
+	pub flags: u32,
 	pub offset: u64,
 	pub size_in_file: u64,
 	pub virtual_address: u64,
@@ -58,6 +63,7 @@ pub struct ProgramHeader {
 	pub alignment: u64,
 }
 
+#[derive(Debug)]
 pub struct SectionHeaderFlags(u64);
 
 impl SectionHeaderFlags {
@@ -155,7 +161,8 @@ impl std::ops::Not for SectionHeaderFlags {
 	}
 }
 
-pub struct SectionHeaderType(i32);
+#[derive(Debug)]
+pub struct SectionHeaderType(u32);
 
 impl SectionHeaderType {
 	pub const SHT_NULL: Self = Self(0x0);
@@ -178,21 +185,34 @@ impl SectionHeaderType {
 	pub const SHT_NUM: Self = Self(0x13);
 }
 
-pub struct SectionHeader {
-	pub name: String,
+pub struct PartialSectionHeader {
 	pub name_offset: u32,
 	pub ty: SectionHeaderType,
 	pub flags: SectionHeaderFlags,
+	pub virtual_address: u64,
 	pub offset: u64,
 	pub size: u64,
-	pub virtual_address: u64,
-	pub alignment: u64,
 	pub link: u32,
 	pub info: u32,
+	pub alignment: u64,
 	pub entry_size: u64,
 }
 
 #[derive(Debug)]
+pub struct SectionHeader {
+	pub name: String,
+	pub ty: SectionHeaderType,
+	pub flags: SectionHeaderFlags,
+	pub virtual_address: u64,
+	pub offset: u64,
+	pub size: u64,
+	pub link: u32,
+	pub info: u32,
+	pub alignment: u64,
+	pub entry_size: u64,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct ElfType(u16);
 
 impl ElfType {
@@ -203,27 +223,28 @@ impl ElfType {
 	pub const ET_CORE: Self = Self(0x04);
 }
 
+#[derive(Debug)]
 pub struct ElfFile {
-	pub version: i32,
+	pub version: u32,
 	pub class: Class,
 	pub endianness: Endianness,
 	pub ty: ElfType,
 	pub isa: ISA,
-	pub abi: i32,
-	pub abi_version: i32,
-	pub flags: i32,
-	pub entry: u64,
-	pub section_name_entry_index: u32,
-	pub header_size: u32,
+	pub abi: u8,
+	pub abi_version: u8,
+	pub flags: u32,
+	pub entrypoint: u64,
+	pub section_name_string_table_index: u16,
+	pub header_size: u16,
 
 	pub program_header_table_offset: u64,
-	pub program_header_table_entry_count: u32,
-	pub program_header_table_entry_size: u32,
+	pub program_header_table_entry_count: u16,
+	pub program_header_table_entry_size: u16,
 	pub program_headers: Vec<ProgramHeader>,
 
 	pub section_header_table_offset: u64,
-	pub section_header_table_entry_count: u32,
-	pub section_header_table_entry_size: u32,
+	pub section_header_table_entry_count: u16,
+	pub section_header_table_entry_size: u16,
 	pub section_headers: Vec<SectionHeader>,
 }
 
@@ -278,7 +299,9 @@ impl ElfFile {
 		cursor.seek_relative(7)?; // skip padding
 
 		let ty = ElfType(cursor.read_16(endianness)?);
-		let isa = ISA::parse(&mut cursor)?;
+		assert_eq!(ty, ElfType::ET_EXEC);
+
+		let isa = ISA::parse(&mut cursor, endianness)?;
 		let version = cursor.read_32(endianness)?;
 		if version != 1 {
 			return Err(std::io::Error::new(
@@ -291,33 +314,106 @@ impl ElfFile {
 		let program_header_table_offset = cursor.read_64(endianness)?;
 		let section_header_table_offset = cursor.read_64(endianness)?;
 		let flags = cursor.read_32(endianness)?;
-		let header_size = cursor.read_32(endianness)?;
+		let header_size = cursor.read_16(endianness)?;
 
 		let program_header_table_entry_size = cursor.read_16(endianness)?;
 		let program_header_table_entry_count = cursor.read_16(endianness)?;
 		let section_header_table_entry_size = cursor.read_16(endianness)?;
 		let section_header_table_entry_count = cursor.read_16(endianness)?;
-		let section_header_table_index = cursor.read_16(endianness)?;
+		let section_name_string_table_index = cursor.read_16(endianness)?;
 
-		dbg!(
+		let mut program_headers: Vec<ProgramHeader> = Vec::new();
+		cursor.set_position(program_header_table_offset);
+
+		for _ in 0..program_header_table_entry_count {
+			program_headers.push(ProgramHeader {
+				ty: ProgramHeaderType(cursor.read_32(endianness)?),
+				flags: cursor.read_32(endianness)?,
+				offset: cursor.read_64(endianness)?,
+				virtual_address: cursor.read_64(endianness)?,
+				physical_address: cursor.read_64(endianness)?,
+				size_in_file: cursor.read_64(endianness)?,
+				size_in_memory: cursor.read_64(endianness)?,
+				alignment: cursor.read_64(endianness)?,
+			});
+
+			// minimum entry size for elf64
+			cursor.seek_relative((program_header_table_entry_size.cast_signed() as i64).saturating_sub(0x38))?;
+		}
+
+		let mut section_headers: Vec<PartialSectionHeader> = Vec::new();
+		cursor.set_position(section_header_table_offset);
+
+		for _ in 0..section_header_table_entry_count {
+			section_headers.push(PartialSectionHeader {
+				name_offset: cursor.read_32(endianness)?,
+				ty: SectionHeaderType(cursor.read_32(endianness)?),
+				flags: SectionHeaderFlags(cursor.read_64(endianness)?),
+				virtual_address: cursor.read_64(endianness)?,
+				offset: cursor.read_64(endianness)?,
+				size: cursor.read_64(endianness)?,
+				link: cursor.read_32(endianness)?,
+				info: cursor.read_32(endianness)?,
+				alignment: cursor.read_64(endianness)?,
+				entry_size: cursor.read_64(endianness)?,
+			});
+
+			// minimum entry size for elf64
+			cursor.seek_relative((section_header_table_entry_size.cast_signed() as i64).saturating_sub(0x40))?;
+		}
+
+		let string_table_section_offset = section_headers[section_name_string_table_index as usize].offset;
+
+		Ok(ElfFile {
+			version,
 			class,
 			endianness,
 			ty,
 			isa,
-			version,
-			entrypoint,
-			program_header_table_offset,
-			section_header_table_offset,
+			abi,
+			abi_version,
 			flags,
+			entrypoint,
+			section_name_string_table_index,
 			header_size,
-			program_header_table_entry_size,
+			program_header_table_offset,
 			program_header_table_entry_count,
-			section_header_table_entry_size,
+			program_header_table_entry_size,
+			program_headers,
+			section_header_table_offset,
 			section_header_table_entry_count,
-			section_header_table_index
-		);
+			section_header_table_entry_size,
+			section_headers: section_headers
+				.into_iter()
+				.map(|header| {
+					let name_index = string_table_section_offset + header.name_offset as u64;
+					cursor.set_position(name_index);
 
-		todo!()
+					let mut name = String::new();
+					loop {
+						let byte = cursor.read_u8()?;
+						if byte == b'\0' {
+							break;
+						}
+
+						name.push(byte as char); // something something this isnt correct probably
+					}
+
+					Ok(SectionHeader {
+						name,
+						ty: header.ty,
+						flags: header.flags,
+						virtual_address: header.virtual_address,
+						offset: header.offset,
+						size: header.size,
+						link: header.link,
+						info: header.info,
+						alignment: header.alignment,
+						entry_size: header.entry_size,
+					})
+				})
+				.collect::<Result<Vec<SectionHeader>, std::io::Error>>()?,
+		})
 	}
 }
 
@@ -327,7 +423,8 @@ mod tests {
 
 	#[test]
 	fn test_elf_file() {
-		let data = include_bytes!("../../../target/boot.o");
+		let data = include_bytes!("../../../target/out.elf");
 		let elf = ElfFile::parse(data).unwrap();
+		dbg!(elf);
 	}
 }

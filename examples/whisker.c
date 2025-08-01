becab483d1cb9bda69e64132531dae3e8be8786b
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2025-08-01 14:21:52 -0400

    add more UART defines in examples

--- examples/whisker.c
+++ examples/whisker.c
@@ -1,87 +1,5 @@
 #include "whisker.h"
 
-/*
-int64_t int_mul(int64_t lhs, int64_t rhs) {
-    // considers each bit in rhs, and if it is set, adds
-    // lhs * bit_value to the accumulator
-    // this works for signed values since they are represented with 2s compliment.
-    // however this is implemented in assembly to get those semantics
-    // and to improve speed of the algorithm even without optimizations.
-    int64_t tmp = 0;
-    int64_t acc = 0;
-    __asm__(
-        "2: andi %[tmp], %[rhs], 1 \n\t"
-        "beqz %[tmp], 3f \n\t" // no bit set
-        "add %[acc], %[acc], %[lhs] \n\t"
-        "3: srli %[rhs], %[rhs], 1 \n\t" // move to next bit
-        "slli %[lhs], %[lhs], 1 \n\t" // increase the value to add for that bit
-        "bnez %[rhs], 2b \n\t"
-        :
-            [acc] "+&r" (acc),
-            [tmp] "+&r" (tmp),
-            [lhs] "+&r" (lhs),
-            [rhs] "+&r" (rhs)
-    );
-    return acc;
-}
-
-void mulwide(int64_t lhs, int64_t rhs, int64_t *restrict hi, int64_t *restrict lo) {
-    // calculates a 64x64=128 bit mul by doing 4 32x32=64 bit multiplications
-    // https://web.archive.org/web/20250222014334/https://stackoverflow.com/questions/26852435/reasonably-portable-way-to-get-top-64-bits-from-64x64-bit-multiply/26855440#26855440
-    uint64_t a = lhs >> 32;
-    uint64_t b = lhs & 0xFFFFFFFF;
-    uint64_t c = rhs >> 32;
-    uint64_t d = rhs & 0xFFFFFFFF;
-
-    uint64_t ac = int_mul(a, c);
-    uint64_t bc = int_mul(b, c);
-    uint64_t ad = int_mul(a, d);
-    uint64_t bd = int_mul(b, d);
-
-    uint64_t mid = (bd >> 32) + (bc & 0xFFFFFFFF) + (ad & 0xFFFFFFFF);
-    *hi = ac + (bc >> 32) + (ad >> 32) + (mid >> 32);
-    *lo = (mid << 32) | (bd & 0xffffffff);
-}
-
-void div_10(int64_t lhs, int64_t *restrict quot, int64_t *restrict rem) {
-    int64_t hi;
-    int64_t lo;
-    mulwide(lhs, 0x6666666666666667, &hi, &lo);
-    int64_t q = hi >> 2;
-    if(lhs < 0){
-        q += 1;
-    }
-    *quot = q;
-    // multiply by 10 to get the remainder
-    *rem = lhs - (((q << 2) + q) << 1);
-}
-
-// dont pass in int64_t::MIN
-void int_div(int64_t lhs, int64_t rhs, int64_t* quotient, int64_t* remainder){
-    // get final sign for division, and get absolute values
-    int sign = 0;
-    if(lhs < 0){
-        sign ^= 1;
-        lhs = -lhs;
-    }
-    if(rhs < 0){
-        sign^= 1;
-        rhs = -rhs;
-    }
-    int64_t quot = 0;
-    int64_t rem = lhs;
-    while(rem >= rhs){
-        rem -= rhs;
-        quot += 1;
-    }
-    if(sign){
-        quot = -quot;
-    }
-    *quotient = quot;
-    *remainder = rem;
-}
-*/
-
 int32_t whisker_strlen(const char *str) {
   int32_t len = 0;
 
@@ -98,7 +16,7 @@ int32_t whisker_strlen(const char *str) {
 void whisker_write_uart(const char *str) {
   int32_t len = whisker_strlen(str);
   for (int32_t i = 0; i < len; i++) {
-    *UART = str[i];
+    *UART_DATA = str[i];
   }
 }
 
--- examples/whisker.h
+++ examples/whisker.h
@@ -2,21 +2,27 @@
 #define WHISKER_H
 #include <stdint.h>
 
-static char *UART = (char *)0x10000000;
 
-/*
-// if lhs or rhs are int64_t::MIN, behavior is undefined
-// if the computation of lhs * rhs overflows, behavior is undefined
-int64_t int_mul(int64_t lhs, int64_t rhs);
+#define UART_BASE ((volatile char *)0x10000000)
+#define UART_DATA UART_BASE
+#define UART_DIVISOR_LSB UART_BASE
+#define UART_INTERRUPT_ENABLE (UART_BASE + 1)
+#define UART_DIVISOR_MSB (UART_BASE + 1)
+#define UART_INTERRUPT_ID (UART_BASE + 2)
+#define UART_FIFO_CTRL (UART_BASE + 2)
+#define UART_LINE_CTRL (UART_BASE + 3)
+#define UART_LINE_STATUS (UART_BASE + 5)
+#define UART_MODEM_STATUS (UART_BASE + 6)
+#define UART_SCRATCH (UART_BASE + 7)
 
-void mulwide(int64_t lhs, int64_t rhs, int64_t *restrict hi, int64_t *restrict lo);
-
-void div_10(int64_t lhs, int64_t *restrict quot, int64_t *restrict rem);
-
-// lhs and rhs must not be int64_t::MIN or behavior is undefined
-// if lhs or rhs are negative, the remainder will likely not be useful
-void int_div(int64_t lhs, int64_t rhs, int64_t* quotient, int64_t* remainder);
-*/
+#define UART_DATA_AVAILABLE_BIT (1 << 0)
+#define UART_OVERRUN_ERR_BIT (1 << 1)
+#define UART_PARITY_ERR_BIT (1 << 2)
+#define UART_FRAMING_ERR_BIT (1 << 3)
+#define UART_BRK_BIT (1 << 4)
+#define UART_THR_EMPTY_BIT (1 << 5)
+#define UART_THR_IDLE_BIT (1 << 6)
+#define UART_FIFO_ERR_BIT (1 << 7)
 
 // str must be nul-terminated, and it should be free'd by the caller if
 // necessary if str is not nul-terminated the program will go into an infinite
